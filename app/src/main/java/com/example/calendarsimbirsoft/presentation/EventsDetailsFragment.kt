d8fb15cf4ef3e9fe6a9d92cf4a2c7bbb235eafff
sobolenkoo <sobolenkoo.aleksandr@gmail.com>
2024-01-11 21:13:58 +0300

    Implemented data transfer between fragments, saving data that has been changed
    Improving performance Jetpack Navigation
    Implemented correct display of time in EventsDetailsFragment
    Implemented transfer of date information from CalendarView to EventsDetailsFragment

--- app/src/main/java/com/example/calendarsimbirsoft/presentation/EventsDetailsFragment.kt
+++ app/src/main/java/com/example/calendarsimbirsoft/presentation/EventsDetailsFragment.kt
@@ -2,33 +2,78 @@ package com.example.calendarsimbirsoft.presentation
 
 import android.os.Bundle
 import android.view.View
+import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.example.calendarsimbirsoft.R
 import com.example.calendarsimbirsoft.databinding.EventsDetailsFragmentBinding
+import com.example.calendarsimbirsoft.presentation.viewModel.EventsViewModel
+import com.example.calendarsimbirsoft.presentation.viewModel.Navigation
+import kotlinx.coroutines.launch
 import kotlinx.datetime.Instant
 import kotlinx.datetime.TimeZone
 import kotlinx.datetime.toJavaLocalDateTime
 import kotlinx.datetime.toLocalDateTime
+import org.koin.androidx.viewmodel.ext.android.sharedViewModel
 import java.time.format.DateTimeFormatter
 import java.util.Locale
+
 
 class EventsDetailsFragment : Fragment(R.layout.events_details_fragment) {
     private val binding: EventsDetailsFragmentBinding by viewBinding()
     private val arguments: EventsDetailsFragmentArgs by navArgs()
+    private val viewModel: EventsViewModel by sharedViewModel()
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         bindViews()
+        initViews()
     }
 
-    private fun bindViews() {
+    private fun initViews() {
         val eventUI = arguments.eventsUI
+        val dateTimeFormatter = DateTimeFormatter.ofPattern("dd MMMM yyyy", Locale("ru"))
         with(binding) {
             title.setText(eventUI.name)
+            date.text = Instant.fromEpochMilliseconds(arguments.date).toLocalDateTime(TimeZone.UTC)
+                .toJavaLocalDateTime().format(dateTimeFormatter)
             "Начало - ${eventUI.dateStart}".also { dateStart.text = it }
             "Конец - ${eventUI.dateFinish}".also { dateFinish.text = it }
             description.setText(eventUI.description)
+        }
+    }
+
+    private fun bindViews() {
+        with(binding) {
+            title.doOnTextChanged { titleText, _, _, _ ->
+                viewModel.updateTitleText(titleText.toString())
+            }
+
+            description.doOnTextChanged { descriptionText, _, _, _ ->
+                viewModel.updateDescriptionText(descriptionText.toString())
+            }
+
+            btnConfirm.setOnClickListener {
+                viewLifecycleOwner.lifecycleScope.launch {
+                    viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                        viewModel.navigationItem.collect { item ->
+                            when (item) {
+                                is Navigation.EventsDetails -> Unit
+                                is Navigation.Pop -> findNavController().popBackStack()
+                            }
+                        }
+                    }
+                }
+                viewModel.onBtnConfirmClicked()
+            }
+
+            btnBackStack.setOnClickListener {
+                findNavController().popBackStack()
+            }
         }
     }
 }
--- app/src/main/java/com/example/calendarsimbirsoft/presentation/viewModel/EventsViewModel.kt
+++ app/src/main/java/com/example/calendarsimbirsoft/presentation/viewModel/EventsViewModel.kt
@@ -6,10 +6,19 @@ import com.example.calendarsimbirsoft.data.EventsRepository
 import com.example.calendarsimbirsoft.domain.NetworkResults
 import com.example.calendarsimbirsoft.presentation.EventsMapper
 import com.example.calendarsimbirsoft.presentation.EventsUI
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharedFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
+
+
+sealed interface Navigation {
+    data class EventsDetails(val eventsUI: EventsUI, val date: Long) : Navigation
+    object Pop : Navigation
+}
 
 class EventsViewModel(
     private val repository: EventsRepository,
@@ -19,6 +28,14 @@ class EventsViewModel(
 
     private val _currentEvents = MutableStateFlow<List<EventsUI>>(emptyList())
     val currentEvents: StateFlow<List<EventsUI>> = _currentEvents.asStateFlow()
+
+    private val _currentDate = MutableStateFlow<Long>(System.currentTimeMillis())
+    val currentDate: StateFlow<Long> = _currentDate.asStateFlow()
+
+    private val _navigationItem = MutableSharedFlow<Navigation>()
+    val navigationItem: SharedFlow<Navigation> = _navigationItem.asSharedFlow()
+
+    private val _currentEvent: MutableStateFlow<EventsUI?> = MutableStateFlow(null)
 
     init {
         receiveEventsData()
@@ -36,5 +53,42 @@ class EventsViewModel(
                 is NetworkResults.Error -> {}
             }
         }
+    }
+
+    fun setCurrentDate(date: Long) {
+        _currentDate.value = date
+    }
+
+    fun updateTitleText(text: String) {
+        _currentEvent.value = _currentEvent.value?.copy(name = text)
+    }
+
+    fun updateDescriptionText(text: String) {
+        _currentEvent.value = _currentEvent.value?.copy(description = text)
+    }
+
+    fun onEventClick(eventsUI: EventsUI) {
+        viewModelScope.launch {
+            _navigationItem.emit(Navigation.EventsDetails(eventsUI, _currentDate.value))
+            _currentEvent.value = eventsUI
+        }
+    }
+
+    fun onBtnConfirmClicked() {
+        viewModelScope.launch {
+            _navigationItem.emit(Navigation.Pop)
+            val currentEvent = _currentEvent.value
+            val currentEvents = _currentEvents.value
+            val changedEvents = currentEvents.map { event ->
+                if (event.id == currentEvent?.id) {
+                    currentEvent
+                } else {
+                    event
+                }
+            }
+            _currentEvent.value = null
+            _currentEvents.value = changedEvents
+        }
+
     }
 }
